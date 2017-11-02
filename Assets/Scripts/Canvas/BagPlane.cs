7be4bb4a33871e54da1f002b191d2e7d5e0a5a2c
周思昊 <l>
2017-11-02 21:27:03 +0800

    add UI

---
+++ Assets/Scripts/Canvas/BagPlane.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagPlane : MonoBehaviour {
+    public GameObject BagPlaneSelf;
+
+    void Awake()
+    {
+        BagPlaneSelf = this.gameObject;
+    }
+
+	// Use this for initialization
+	void Start () {
+        BagPlaneSelf.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool isPanelOpen { get { return BagPlaneSelf.activeSelf; } }
+
+    public void OnClick()
+    {
+        if (isPanelOpen)
+            BagPlaneSelf.SetActive(false);
+        else
+            BagPlaneSelf.SetActive(true);
+    }
+}
